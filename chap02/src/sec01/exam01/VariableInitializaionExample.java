ce25588cb72e67832116356580072783739ac46f
gyujun9403 <75626815+gyujun9403@users.noreply.github.com>
2020-12-17 10:21:39 +0900

    12.17 혼공자 실습

---
+++ chap02/src/sec01/exam01/VariableInitializaionExample.java
@@ -0,0 +1,13 @@
+package sec01.exam01;
+
+public class VariableInitializaionExample {
+
+	public static void main(String[] args) {
+		int value = 10;
+		int result = value + 10;
+		
+		System.out.println(result);
+
+	}
+
+}
