1bb9a5a450be0beff6ca53d5d0e5a9a044d2ef99
gyujun9403 <75626815+gyujun9403@users.noreply.github.com>
2020-12-20 15:22:13 +0900

    12.20 혼공자 실습

---
+++ chap02/src/sec03/exam05/OperationPromotionExample.java
@@ -0,0 +1,14 @@
+package sec03.exam05;
+
+public class OperationPromotionExample {
+
+	public static void main(String[] args) {
+		char charvalue1 = 'A';
+		char charvalue2 = 1;
+		int intvalue1 = charvalue1 + charvalue2; 
+		System.out.println("유니코드 : " + intvalue1);
+		System.out.println("출력문자 : " + (char)intvalue1);
+
+	}
+
+}
