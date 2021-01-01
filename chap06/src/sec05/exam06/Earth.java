99bb498f68cebbd8693691c72105d9690ded3beb
gyujun9403 <75626815+gyujun9403@users.noreply.github.com>
2021-01-01 23:20:45 +0900

    01.01 혼공자_staticfinal(상수) 실습

---
+++ chap06/src/sec05/exam06/Earth.java
@@ -0,0 +1,7 @@
+package sec05.exam06;
+
+public class Earth {
+	//상수는 항상 대문자로 선언.
+	static final double EARTH_RATIUS = 6400;
+	static final double REATH_AREA = 4*Math.PI * EARTH_RATIUS * EARTH_RATIUS;
+}
---
+++ chap06/src/sec05/exam06/EarthExample.java
@@ -0,0 +1,10 @@
+package sec05.exam06;
+
+public class EarthExample {
+
+	public static void main(String[] args) {
+		System.out.println("지구의 반지름은 " + Earth.EARTH_RATIUS + "km 이다.");
+		System.out.println("지구의 표면적은 " + Earth.REATH_AREA + "km^2 이다.");
+	}
+
+}
