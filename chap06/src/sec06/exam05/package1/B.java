9b66149e58a4fe07bedcb7068c328a233e1aa7e7
gyujun9403 <75626815+gyujun9403@users.noreply.github.com>
2021-01-02 22:31:43 +0900

    01.02 혼공자_필드메소드접근제한 실습

---
+++ chap06/src/sec06/exam05/package1/B.java
@@ -0,0 +1,14 @@
+package sec06.exam05.package1;
+
+public class B {
+	/*
+	 A a = new A();
+	 a.field1 = 1;
+	 Syntax error on token "i", VariableDeclaratorId expected after this token
+	*/
+	public B() {
+		A a = new A();
+		a.field1 = 1;
+	}
+	
+}
---
+++ chap06/src/sec06/exam05/package2/C.java
@@ -0,0 +1,16 @@
+package sec06.exam05.package2;
+
+import sec06.exam05.package1.A;
+
+public class C {
+	public C () {
+		A a = new A();
+		a.field1 = 1;
+		//a.field2 = 1;	The field A.field2 is not visible
+		//a.field3 = 1;	The field A.field3 is not visible
+		
+		a.method1();
+		//a.method2();	The method method2() from the type A is not visible
+		//a.method3();	The method method3() from the type A is not visible
+	}
+}
