5582961ae05f4ab1f46f8a1d4f951d7b04805c36
gyujun9403 <75626815+gyujun9403@users.noreply.github.com>
2021-01-04 23:26:28 +0900

    01.04 혼공자_클래스자동타입변환 실습

---
+++ chap07/src/sec02/exam02/ChildExample.java
@@ -0,0 +1,11 @@
+package sec02.exam02;
+
+public class ChildExample {
+
+	public static void main(String[] args) {
+		Parent parent = new Child();
+		parent.method1();
+		parent.method2();
+	}
+
+}
---
+++ chap07/src/sec02/exam02/Parent.java
@@ -0,0 +1,6 @@
+package sec02.exam02;
+
+public class Parent {
+	public void method1() {System.out.println("Parent_method1");}
+	public void method2() {System.out.println("Parent_method2");}
+}
