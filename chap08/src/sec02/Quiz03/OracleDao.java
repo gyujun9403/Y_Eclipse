acfa8dbd0d8bef767bb0541292b55d2bc62e77de
gyujun9403 <75626815+gyujun9403@users.noreply.github.com>
2021-01-07 21:40:12 +0900

    01.07 혼공자_인터페이스 퀴즈

---
+++ chap08/src/sec02/Quiz03/OracleDao.java
@@ -0,0 +1,8 @@
+package sec02.Quiz03;
+
+public class OracleDao implements DataAccessObject{
+	public void select() {System.out.println("Oracle DB에서 검색");}
+	public void insert() {System.out.println("Oracle DB에서 삽입");}
+	public void update() {System.out.println("Oracle DB에서 수정");}
+	public void delete() {System.out.println("Oracle DB에서 삭제");}
+}
